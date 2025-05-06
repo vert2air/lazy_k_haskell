4e577780dd00cfe7d8ca2de24330447b8f8bea6a
vert2air <vert2air@gmail.com>
2025-05-06 14:47:40 +0900

    refactor : getNchar と pollInput 見直し
    On branch feature_lazy_more
    Changes to be committed:
    	modified:   src/LazyKParts.hs

--- src/LazyKParts.hs
+++ src/LazyKParts.hs
@@ -120,24 +120,23 @@ reductInput ioInf d expr = do
 pollInput :: Int     -- ^ 何番目のbyteまで取得するか。0オリジン。
         -> IoInfo    -- ^ 入力情報と出力関係のオプション
         -> IO IoInfo -- ^ 新たに入力されたbyteを反映した IoInfo
-pollInput ix (IoInfo _ input _ pd sgn start) = do
-    IoInfo eof' add _ _ _ _ <- getNchar [] $ ix - length input + 1
+-- pollInput ix (IoInfo _ input _ pd sgn start) = do
+pollInput ix ioInf = do
+    (eof', add) <- getNchar [] $ ix - length (inHist ioInf) + 1
     -- putStrLn $ "------> getNchar !! " ++ show (length input) ++ ".. = " ++ show add
     -- putStrLn $ "                " ++ show (input ++ add)
-    return $ IoInfo eof' (input ++ add) False pd sgn start
+    return $ ioInf { inEof = eof', inHist = inHist ioInf ++ add }
 
 -- | pollInput の補助関数。指定byte数を取得する。
---
--- 入力に関するフィールド以外は呼び出し側で上書きするので、
--- ここではdefault値等を設定しておけばよ良い。
-getNchar :: [Int]     -- ^ pollInputでここまでに受信したbyte列。
+-- inputが EOF に達したかの情報も合わせて返却する。
+getNchar :: [Int]     -- ^ getNcharでここまでに受信したbyte列。
         -> Int        -- ^ 取得すべき残りのbyte数。
-        -> IO IoInfo
+        -> IO (Bool, [Int])
 getNchar acc n
-    | n <= 0 = return $ IoInfo False acc False def 'λ' 0
+    | n <= 0 = return (False, acc)
     | otherwise = do
         eof <- isEOF
-        if eof then return $ IoInfo True acc False def 'λ' 0
+        if eof then return (True, acc)
               else do
                   c <- getChar  -- 実際の読込み。それまではblocking。
                   getNchar (acc ++ [ord c]) (n - 1)
