8a0f3697d891598fe83564b6ff0d584ca6377d83
vert2air <vert2air@gmail.com>
2025-05-25 19:53:17 +0900

    add : test向けの修正と、Test拡充
    lazyがテストしやすいように、構造も変更。
    
    On branch feature_add_tests
    Changes to be committed:
    	modified:   app/lazy.hs
    	modified:   src/LazyKParts.hs
    	modified:   test/HUnitTests.hs

--- test/HUnitTests.hs
+++ test/HUnitTests.hs
@@ -1,14 +1,21 @@
 module HUnitTests where
 
 import Test.HUnit
+import Data.Char (ord)
+import Data.Default (Default(..))
+import Data.Either (fromRight)
 
-import LamCalcCore (reductInf, toLambda, readLazyK)
+import LamCalcCore (LamExpr(..), IoInfo(..)
+                  , (%:), reductInf, toLambda, readLazyK)
 import LamCalcParts (getChNum)
+import LazyKParts (deconsLoopCc)
 import ShortChurchNum (shortChNum)
 
 hUnitAll :: IO Counts
 hUnitAll = do
     runTestTT tests
+    runTestTT $ TestList [test_lazy]
+    runTestTT $ TestList [test_add_A_B]
   where
     tests = TestList $ map (\n ->
         let title = ("Church number Test :" ++ show n)
@@ -22,3 +29,18 @@ calc :: String -> Int -> Either String Int
 calc title n = case readLazyK title . (shortChNum!!) $ n of
             Right e  -> getChNum . reductInf . toLambda $ e
             Left msg -> Left msg
+
+test_lazy :: Test
+test_lazy = TestCase $ do
+    src <- readFile "lazy/prime_numbers.lazy"
+    let expr = fromRight (Nm "dummy") . readLazyK "doctest" $ src
+    (_, out) <- deconsLoopCc def def (Just 6) $ expr %: In(0)
+    assertEqual "lazy deconsLoopCc" out $ map ord "2 3 5 "
+
+test_add_A_B :: Test
+test_add_A_B = TestCase $ do
+    src <- readFile "lazy/add_A_B.lazy"
+    let expr = fromRight (Nm "dummy") . readLazyK "doctest" $ src
+    let ioInf = def {inEof = True, inHist = [7, 11]}
+    (_, out) <- deconsLoopCc ioInf def Nothing $ expr %: In(0)
+    assertEqual "add_A_B deconsLoopCc" out [18]
