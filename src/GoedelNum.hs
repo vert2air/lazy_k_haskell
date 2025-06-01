8102aa736700886640a98ea8f91a2ada37f834ca
vert2air <vert2air@gmail.com>
2025-06-02 00:05:42 +0900

    snapshot : Goedel数->CC式の高速化 snapshot
    cover_struct の調整で行けそうな気もするが、一旦commit.
    まだ、cabal test を通っていない。
    
    On branch feature_fast_goedel
    Changes to be committed:
    	modified:   src/GoedelNum.hs

--- src/GoedelNum.hs
+++ src/GoedelNum.hs
@@ -1,5 +1,6 @@
 module GoedelNum where
 
+import Debug.Trace (trace)
 import qualified Data.Map as M (Map, fromList, lookupLE)
 
 import LamCalcCore (LamExpr(..), (%:))
@@ -17,16 +18,26 @@ mk_rank_sum k
 
 変数が0個の場合、goedel数の個数も 0個。
 変数が1個の場合、goedel数は 1 から 3 までの 3個。
+
+>>> take 10 rank_sum
+[0,3,9,54,405,3402,30618,288684,2814669,28146690]
 -}
 rank_sum :: [Integer]
 rank_sum = map mk_rank_sum [0..]
 
--- | 変数の個数毎のgoedel番号の個数を累積和にしたもの
+{- | 変数の個数毎のgoedel番号の個数を累積和にしたもの
+
+>>> take 10 rank_cum
+[0,3,12,66,471,3873,34491,323175,3137844,31284534]
+-}
 rank_cum :: [Integer]
 rank_cum = (0:) $ zipWith (+) rank_cum $ drop 1 rank_sum
 
-gn_struct :: [(Integer, (Int, Int))]
-gn_struct = zip gn_struct_bound gn_struct_pair
+-- gn_struct :: [(Integer, (Int, Int))]
+-- gn_struct = zip gn_struct_bound gn_struct_pair
+
+gn_struct :: [(Integer, (Integer, Int, Int))]
+gn_struct = zip gn_struct_ix_bound gn_struct_ix_pair
 
 {- | gn_struct_bound と対応して、変数の個数の組合せ
 
@@ -35,6 +46,71 @@ gn_struct = zip gn_struct_bound gn_struct_pair
 gn_struct_pair :: [(Int, Int)]
 gn_struct_pair = ((1, 0):) . concat $
         map (\r -> map (\n -> (n, r - n)) [1..r - 1]) [1..]
+
+{- | gn_struct_ix_bound と対応して、以下のtripleのリストを生成
+  (関数適用の関数部が何番目か, 関数部の変数数, 引数部の変数数)
+
+0番目は、変数が 1 個で、まだ関数適用がないので、取り合えず (0, 1, 0) としておく。
+
+>>> gn_struct_ix_pair !! 0  -- 足して1は分解できないが、形式上置いておく。
+(0,1,0)
+>>> gn_struct_ix_pair !! 1  -- 足して2は、(1,1) のみ。1個は、3パターン
+(0,1,1)
+>>> gn_struct_ix_pair !! 2
+(1,1,1)
+>>> gn_struct_ix_pair !! 3
+(2,1,1)
+>>> gn_struct_ix_pair !! 4  -- 足して3は、(1,2) と (2,1)。
+(0,1,2)
+>>> gn_struct_ix_pair !! 5  -- 足して3は、(1,2) と (2,1)。
+(1,1,2)
+>>> gn_struct_ix_pair !! 6  -- 足して3は、(1,2) と (2,1)。
+(2,1,2)
+>>> gn_struct_ix_pair !! 7  --   (2,1) は、f側が 9個 のバリエーション。
+(0,2,1)
+>>> gn_struct_ix_pair !! 8
+(1,2,1)
+>>> gn_struct_ix_pair !! 9
+(2,2,1)
+>>> gn_struct_ix_pair !! 10
+(3,2,1)
+>>> gn_struct_ix_pair !! 11
+(4,2,1)
+>>> gn_struct_ix_pair !! 12
+(5,2,1)
+>>> gn_struct_ix_pair !! 13
+(6,2,1)
+>>> gn_struct_ix_pair !! 14
+(7,2,1)
+>>> gn_struct_ix_pair !! 15
+(8,2,1)
+
+>>> gn_struct_ix_pair !! 16  -- 足して4は、(1,3), (2,2), (3,1)。
+(0,1,3)
+>>> gn_struct_ix_pair !! 17
+(1,1,3)
+>>> gn_struct_ix_pair !! 18
+(2,1,3)
+
+>>> gn_struct_ix_pair !! 19  --   (2,2) は、f側が 9個 のバリエーション。
+(0,2,2)
+>>> gn_struct_ix_pair !! 20
+(1,2,2)
+
+>>> gn_struct_ix_pair !! 28 --   (3,1) は、f側が 0, 1, 2 のバリエーション。
+(0,3,1)
+>>> gn_struct_ix_pair !! 29
+(1,3,1)
+>>> gn_struct_ix_pair !! 30
+(2,3,1)
+-}
+gn_struct_ix_pair :: [(Integer, Int, Int)]
+gn_struct_ix_pair = ((0, 1, 0):) . concat . concat $
+    flip map [1..] $ \r ->
+        flip map [1..r-1] $ \n ->
+            flip map [1..(rank_sum !! n)] $ \i ->
+                (i - 1, n, r - n)
+
 
 {- | 最上位の関数適用の関数と引数に含まれる変数の個数の組み合わせが変わる境界値
 
@@ -49,31 +125,104 @@ gn_struct_bound = (0:) . (rank_sum!!1:) $
     off f r = let r_1 = drop 1 . take r $ rank_sum
               in sum . take f . zipWith (*) r_1 . reverse $ r_1
 
--- | Goedel数 gn の式が何個の変数から成るか
---
--- r は再帰用なので、呼出し時は、0 or 1 を指定すること。
+{- | 最上位の関数適用の関数と引数に含まれる変数の個数の組み合わせが変わる境界値
+
+最初の要素は、ゲーデル数が 0　オリジンなので 0。
+2番目の要素は、変数が 1 個の場合の組み合わせ数 3。
+
+>>> gn_struct_ix_bound !! 0  -- (0,1,0)足して1は分解できないが、形式上置いておく。
+0
+>>> gn_struct_ix_bound !! 1  -- (0,1,1)足して2は、(1,1) のみ。3～
+3
+>>> gn_struct_ix_bound !! 2 -- (1,1,1)
+6
+>>> gn_struct_ix_bound !! 3 -- (2,1,1)
+9
+>>> gn_struct_ix_bound !! 4  -- (0,1,2)足して3は、(1,2) と (2,1)。
+12
+>>> gn_struct_ix_bound !! 5  -- (1,1,2)足して3は、(1,2) と (2,1)。
+21
+>>> gn_struct_ix_bound !! 6  -- (2,1,2)足して3は、(1,2) と (2,1)。
+30
+>>> gn_struct_ix_bound !! 7  -- (0,2,1)  (2,1) は、f側が 9個 のバリエーション。
+39
+>>> gn_struct_ix_bound !! 8 -- (1,2,1)
+42
+>>> gn_struct_ix_bound !! 9 -- (2,2,1)
+45
+>>> gn_struct_ix_bound !! 14 -- (7,2,1)
+60
+>>> gn_struct_ix_bound !! 15 -- (8,2,1)
+63
+>>> gn_struct_ix_bound !! 16  -- (0,1,3)足して4は、(1,3), (2,2), (3,1)。
+66
+>>> gn_struct_ix_bound !! 17 -- (1,1,3)
+120
+>>> gn_struct_ix_bound !! 18 -- (2,1,3)
+174
+>>> gn_struct_ix_bound !! 19  -- (0,2,2)  (2,2) は、f側が 9個 のバリエーション。
+228
+-}
+gn_struct_ix_bound :: [Integer]
+-- gn_struct_ix_bound = (0:) . (rank_sum!!1:) $
+--                                    map (calc . (gn_struct_ix_pair !!)) [1..]
+gn_struct_ix_bound = map (calc . (gn_struct_ix_pair !!)) [0..]
+  where
+    calc (i, f, a) = rank_cum !! (f + a - 1)  -- f+a個の変数を使うGoedel数の先頭。
+                  + off f (f + a)   -- (1,f+a-1),(2,f+a-2)..(f-1,a+1)までの個数。
+                  + (rank_sum !! a) * fromIntegral i  -- (f,a) の中で、i対応
+    off f r = let r_1 = drop 1 . take r $ rank_sum
+              in sum . take (f - 1) . zipWith (*) r_1 . reverse $ r_1
+
+{- | Goedel数 gn の式が何個の変数から成るか
+
+r は再帰用なので、呼出し時は、0 or 1 を指定すること。
+
+>>> cover_rank 0 2
+1
+>>> cover_rank 1 2
+1
+>>> cover_rank 0 11
+2
+>>> cover_rank 0 65
+3
+-}
 cover_rank :: Int -> Integer -> Int
-cover_rank r gn = if gn < rank_sum !! r
+cover_rank r gn = if gn < rank_cum !! r
                     then r
                     else cover_rank (r + 1) gn
 
--- | 変数 r 個の式の処理に必要な、gn_struct の個数
-cover_struct :: Int -> Int
-cover_struct r = foldl (+) 0 [0..r]
+{- | 変数 r 個の式の処理に必要な、gn_struct の個数
+
+>>> cover_struct 1
+1
+>>> cover_struct 2
+4
+>>> cover_struct 3
+16
+-}
+cover_struct :: Int -> Integer
+cover_struct n = sum $ for [1..n] $ \r ->
+                  sum $ for [1..r-1] $ \f ->
+                      let a = r - f
+                      in rank_sum !! f
+  where for = flip map
+-- cover_struct r = foldl (\acc i -> acc + i * i) 0 [1..r]
+-- cover_struct r = 4^(r - 1)
 
 goedel_to_expr :: Integer -> LamExpr
 goedel_to_expr gn = g2e_aux (decomp_map gn) gn
 
-decomp_map :: Integer -> M.Map Integer (Int, Int)
+decomp_map :: Integer -> M.Map Integer (Integer, Int, Int)
 decomp_map gn = binSearch o_struct
   where
     o_rank = cover_rank 1 gn
     o_struct = cover_struct o_rank
 
-binSearch :: Int -> M.Map Integer (Int, Int)
-binSearch os = M.fromList $ take os gn_struct
+binSearch :: Integer -> M.Map Integer (Integer, Int, Int)
+binSearch os = M.fromList $ take (fromIntegral os) gn_struct
 
-g2e_aux :: M.Map Integer (Int, Int) -> Integer -> LamExpr
+g2e_aux :: M.Map Integer (Integer, Int, Int) -> Integer -> LamExpr
 g2e_aux _ 0 = Nm "I"
 g2e_aux _ 1 = Nm "K"
 g2e_aux _ 2 = Nm "S"
@@ -81,15 +230,24 @@ g2e_aux decomp gn = g2e_aux decomp f_gn %: g2e_aux decomp o_gn
   where
     (f_gn, o_gn) = decomp_gn decomp gn
 
-decomp_gn :: M.Map Integer (Int, Int) -> Integer -> (Integer, Integer)
+{- | Goedel数を、最上位の関数適用の位置で分割
+
+>>> let decomp = M.fromList $ take 500 gn_struct
+>>> decomp_gn decomp 12   -- `i`ii を i と `ii に分割
+(0,3)
+>>> decomp_gn decomp 39   -- ``iii を `ii と i に分割
+(3,0)
+>>> decomp_gn decomp 66   -- `i`i`ii を i と `i`ii に分割
+(0,12)
+-}
+decomp_gn :: M.Map Integer (Integer, Int, Int) -> Integer -> (Integer, Integer)
 decomp_gn decomp gn = (f_gn, o_gn)
   where
-    (top, (f_cnt, o_cnt)) = maybe (error "Inner Error: decomp_gn") id $
+    (top, (f_ord, f_cnt, o_cnt)) = maybe (error "Inner Error: decomp_gn") id $
                                     M.lookupLE gn decomp
-    renum = gn - top
-    (f_ord, o_ord) = renum `divMod` (rank_sum !! o_cnt)
-    f_gn = f_ord + rank_cum !! (f_cnt - 1)
-    o_gn = o_ord + rank_cum !! (o_cnt - 1)
+    o_ord = gn - top
+    f_gn = fromIntegral f_ord + rank_cum !! (f_cnt - 1)
+    o_gn = o_ord              + rank_cum !! (o_cnt - 1)
 
 expr_to_goedel :: LamExpr -> (Integer, Int)
 expr_to_goedel (Nm "I") = (0, 1)
