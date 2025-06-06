a867d64427ca3b81084a6002195dcaed5d0e86d3
vert2air <vert2air@gmail.com>
2025-06-06 21:24:40 +0900

    add : coverageの為のtest case
    On branch feature_test_enhance
    Changes to be committed:
    	modified:   test/HUnitTests.hs

--- test/HUnitTests.hs
+++ test/HUnitTests.hs
@@ -5,9 +5,12 @@ import Data.Char (ord)
 import Data.Default (Default(..))
 import Data.Either (fromRight)
 import qualified Data.Map as M (fromList)
+import System.Exit (ExitCode(..))
 
-import LamCalcCore (LamExpr(..), IoInfo(..)
-                  , (%:), reductInf, toLambda, readLazyK)
+import LamCalcCore (LamExpr(..), IoInfo(..), NameManager(..)
+                  , (%:), la, reductInf, toLambda, readLazyK, toNamedString
+                  , takeStringified, applyN
+                  )
 import LamCalcParts (Stat(..), getChNum, stat)
 import LazyKParts (deconsLoopCc, deconsLoopLc)
 import GoedelNum (expr_to_goedel)
@@ -18,6 +21,8 @@ hUnitAll = do
     runTestTT $ TestList
         [ test_lazy, test_add_A_B_Cc, test_add_A_B_Lc, test_goedel_err
         , test_church, test_stat
+        , test_show_lamExpr
+        , test_show_NameManager
         ]
 
 test_church :: Test
@@ -38,8 +43,8 @@ test_lazy :: Test
 test_lazy = TestCase $ do
     src <- readFile "lazy/prime_numbers.lazy"
     let expr = fromRight (Nm "dummy") . readLazyK "doctest" $ src
-    (_, out) <- deconsLoopCc def def (Just 6) $ expr %: In(0)
-    assertEqual "lazy deconsLoopCc" out $ map ord "2 3 5 "
+    assertEqual "lazy deconsLoopCc" (ExitSuccess, map ord "2 3 5 ")
+                        =<< deconsLoopCc def def (Just 6) (expr %: In(0))
 
 test_add_A_B_Cc :: Test
 test_add_A_B_Cc = TestCase $ do
@@ -47,9 +52,9 @@ test_add_A_B_Cc = TestCase $ do
     let expr = fromRight (Nm "dummy") . readLazyK "doctest" $ src
     res <- flip mapM [True, False] $ \eof -> do
         let ioInf = def {inEof = eof, inHist = [7, 11]}
-        (_, out) <- deconsLoopCc ioInf def Nothing $ expr %: In(0)
-        return out
-    assertEqual "add_A_B deconsLoopCc" res [[18], [18]]
+        deconsLoopCc ioInf def Nothing $ expr %: In(0)
+    assertEqual "add_A_B deconsLoopCc" res
+                                [(ExitSuccess, [18]), (ExitSuccess, [18])]
 
 test_add_A_B_Lc :: Test
 test_add_A_B_Lc = TestCase $ do
@@ -57,9 +62,9 @@ test_add_A_B_Lc = TestCase $ do
     let expr = fromRight (Nm "dummy") . readLazyK "doctest" $ src
     res <- flip mapM [True, False] $ \eof -> do
         let ioInf = def {inEof = eof, inHist = [7, 11]}
-        (_, out) <- deconsLoopLc ioInf def Nothing $ toLambda expr %: In(0)
-        return out
-    assertEqual "add_A_B deconsLoopCc" res [[18], [18]]
+        deconsLoopLc ioInf def Nothing $ toLambda expr %: In(0)
+    assertEqual "add_A_B deconsLoopLc" res
+                                [(ExitSuccess, [18]), (ExitSuccess, [18])]
 
 test_goedel_err :: Test
 test_goedel_err = TestCase $ do
@@ -87,3 +92,42 @@ test_stat = TestCase $ do
           , church_number = M.fromList [(5,1)]
           }
     assertEqual "stat" stt $ stat 0 expr
+
+test_show_NameManager :: Test
+test_show_NameManager = TestCase $ do
+    assertEqual "NameManager show" (show (def :: NameManager)) $
+        "NameManager {nmPolicy = PK_minimum"
+        ++ ", nmPool = \"xyzabcdefghjlmnopqrtuvwXYZABCDEFGHJLMNOPQRTUVW\""
+        ++ ", nmStack = \"\", nmUnlamStyle = False, nmLamSign = '\\955'}"
+
+test_show_lamExpr :: Test
+test_show_lamExpr = TestCase $ do
+    assertEqual "lamExpr show" "λ X(_1Z)" $ show $
+                la $ Nm "X" %: (V 1 %: Nm "Z")
+    assertEqual "lamExpr show" "S(*Ii)" $ show $ Nm "S" %: (Nm "I" %: Nm "iota")
+    assertEqual "lamExpr show" "*iIS" $ show $ (Nm "iota" %: Nm "I") %: Nm "S"
+    assertEqual "lamExpr show" "*ii" $ show $ Nm "iota" %: Nm "iota"
+    assertEqual "lamExpr show" "*i(*iS)" $ show $ Nm "iota" %: (Nm "iota" %: Nm "S")
+    assertEqual "lamExpr show" "*(*iS)i" $ show $ (Nm "iota" %: Nm "S") %: Nm "iota"
+    assertEqual "lamExpr show" "*i*i*is" $
+                takeStringified $ toNamedString def {nmUnlamStyle = True} $
+                            Nm "iota" %: (Nm "iota" %: (Nm "iota" %: Nm "S"))
+    assertEqual "lamExpr show" "***siii" $
+                takeStringified $ toNamedString def {nmUnlamStyle = True} $
+                            ((Nm "S" %: Nm "iota") %: Nm "iota") %: Nm "iota"
+    assertEqual "lamExpr show" "`*iis" $
+                takeStringified $ toNamedString def {nmUnlamStyle = True} $
+                    (Nm "iota" %: Nm "iota") %: Nm "S"
+    assertEqual "lamExpr show" "`s*ii" $
+                takeStringified $ toNamedString def {nmUnlamStyle = True} $
+                    Nm "S" %: (Nm "iota" %: Nm "iota")
+    assertEqual "lamExpr show" "λx.(λx.x)(λx.x)" $
+                takeStringified $ toNamedString def $
+                la $ (la (V 1) %: la (V 1))
+    assertEqual "lamExpr show"
+        ("λxyzabcdefghjlmnopqrtuvwXYZABCDEFGHJLMNOPQRTUVW."
+        ++ "xyzabcdefghjlmnopqrtuvwXYZABCDEFGHJLMNOPQRTUVW") $
+            takeStringified . toNamedString def .
+                applyN ((26 - 3) * 2) la .
+                    foldl1 (\acc e -> acc %: e) . map V $
+                        [(26 - 3) * 2, (26 - 3) * 2 -1 .. 1]
