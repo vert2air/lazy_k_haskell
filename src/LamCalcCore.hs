f040ab1c76425c63cebdc25e4965a594aa35ba51
vert2air <vert2air@gmail.com>
2025-06-06 19:38:25 +0900

    delete : IoInfo に不要な Ord の deriving
    使っていない deriving は coverage の低下させることもあり、削除。
    
    On branch feature_test_enhance
    Changes to be committed:
    	modified:   src/LamCalcCore.hs

--- src/LamCalcCore.hs
+++ src/LamCalcCore.hs
@@ -532,7 +532,7 @@ data IoInfo = IoInfo
     , progDot :: ProgDot -- ^ 進捗dotを表示すべきの出力頻度。
     , lamSign :: Char -- ^ ラムダ抽象の記号。'λ'か'\\'を想定。
     , startCPUTime :: !Integer -- ^ 開始時の getCPUTime
-    } deriving (Eq, Ord, Show)
+    } deriving (Eq, Show)
 
 instance Default IoInfo where
     def = IoInfo False [] False def 'λ' 0
